bec5fdeb13640d5ba53a5efb765d402f7ac46e87
munkhtsogt <munkhuu48@gmail.com>
2018-02-02 17:41:14 -0600

    login component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+/* MATERIAL MODULES */
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatCardModule, MatTabsModule, MatInputModule } from '@angular/material';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+/* END */
 
+const ROUTES: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginSignupComponent},
+  {path: '***', redirectTo: 'login'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignupComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(ROUTES),
+    /* MATERIAL MODULES */
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTabsModule
+    /* END */
   ],
   providers: [],
   bootstrap: [AppComponent]
